76402ee3f27c1bc92d6db6cdf04bae754ff62548
Sabadashka <90348773+Sabadashka@users.noreply.github.com>
2022-04-28 09:01:22 +0300

    practice3

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Staff_Project
+{
+    using System;
+    using System.Collections.Generic;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Main logic = new Main();
+
+            while (true)
+            {
+                var action = logic.PrintMenu();
+                if (action.Item2 == 0) break; // if exit
+
+                if(action.Item1 == "BaseMenu")
+                {
+                    if (action.Item2 == 1) logic.Login();
+                    else if (action.Item2 == 2) logic.Registration();
+                }
+                else if(action.Item1 == "StaffMenu")
+                {
+                    if (action.Item2 == 1) logic.CreateNewPayment();
+                    else if (action.Item2 == 2) logic.EditPaymentWithDraftStatus();
+                    else if (action.Item2 == 3) logic.DeletePaymentWithDraftStatus();
+                    else if (action.Item2 == 4) logic.PrintAllUserPayments();
+                    else if (action.Item2 == 5) logic.PrintAllUserPaymentsByFilter();
+                    else if (action.Item2 == 6) logic.PrintAllApprovedPayments();
+                    else if (action.Item2 == 7) logic.EditAndSendForModeration();
+                    else if (action.Item2 == 8) logic.LogOut();
+                }
+                else if(action.Item1 == "AdminMenu")
+                {
+                    if (action.Item2 == 1) logic.CreateNewPayment();
+                    else if (action.Item2 == 2) logic.EditPaymentWithDraftStatus();
+                    else if (action.Item2 == 3) logic.DeletePaymentWithDraftStatus();
+                    else if (action.Item2 == 4) logic.PrintAllUserPayments();
+                    else if (action.Item2 == 5) logic.PrintAllUserPaymentsByFilter();
+                    else if (action.Item2 == 6) logic.PrintAllApprovedPayments();
+                    else if (action.Item2 == 7) logic.EditAndSendForModeration();
+                    else if (action.Item2 == 8) logic.LogOut();
+                    else if (action.Item2 == 9) logic.AdminPanelPrintAllPayments();
+                    else if (action.Item2 == 10) logic.AdminPanelPrintAllRejectedPayments();
+                    else if (action.Item2 == 11) logic.AdminPanelSetStatusOfPayment();
+                }
+            }
+        }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,15 @@
+﻿namespace Staff_Project
+{
+    using System.Collections.Generic;
+
+    public abstract class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public Role Role { get; set; }
+        public List<Payment> Payments { get; set; }
+    }
+}
